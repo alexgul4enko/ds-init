d1ac890c024236bb66bdfec817983120cc7f67f8
alexgul4enko <aleksandr.gulchenko@gmail.com>
2020-04-12 13:43:37 +0300

    asda

--- bin/index.js
+++ bin/index.js
@@ -1,39 +1,45 @@
-var chalk = require('chalk');
-
-var semver = require('semver');
-
-var path = require('path');
-
-var program = require('commander');
-
-var init = require('../lib/init');
-
-var requiredVersion = require('../package.json').engines.node;
+const chalk = require('chalk');
+const semver = require('semver');
+const path = require('path');
+const program = require('commander');
+const init = require('../src/init');
+const requiredVersion = require('../package.json').engines.node;
 
 if(!semver.satisfies(process.version, requiredVersion)) {
-  console.log(chalk.red('\nMinimum node version not met :)') + chalk.yellow('\nYou are using Node '.concat(process.version, ', Requirement: Node ').concat(requiredVersion, '.\n')));
+  console.log(
+    chalk.red(`\nMinimum node version not met :)`) +
+      chalk.yellow(
+        `\nYou are using Node ${process.version}, Requirement: Node ${requiredVersion}.\n`,
+      ),
+  );
   process.exit(1);
 }
 
 function wrapCommand(fn) {
-  return function() {
-    return fn.apply(void 0, arguments)['catch'](function(err) {
+  return (...args) =>
+    fn(...args).catch((err) => {
       console.error(chalk.red(err.stack));
       process.exitCode = 1;
     });
-  };
 }
 
-program.version(require('../package.json').version).usage('<command> [options]');
-program.command('init [siteName] [folderName] [rootDir]').description('Initialize website').action(function(siteName, folderName) {
-  var rootDir = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : '.';
-  wrapCommand(init)(path.resolve(rootDir), siteName, folderName);
-});
-program.arguments('<command>').action(function(cmd) {
+program
+  .version(require('../package.json').version)
+  .usage('<command> [options]');
+
+program
+  .command('init [siteName] [folderName] [rootDir]')
+  .description('Initialize website')
+  .action((siteName, folderName, rootDir = '.') => {
+    wrapCommand(init)(path.resolve(rootDir), siteName, folderName);
+  });
+
+program.arguments('<command>').action((cmd) => {
   program.outputHelp();
-  console.log('  '.concat(chalk.red('\n  Unknown command '.concat(chalk.yellow(cmd), '.'))));
+  console.log(`  ${chalk.red(`\n  Unknown command ${chalk.yellow(cmd)}.`)}`);
   console.log();
 });
+
 program.parse(process.argv);
 
 if(!process.argv.slice(2).length) {
